2065a5cc6a309590924febc54149e89c51b7fff5
Aries M. <triknhoj46@gmail.com>
2026-05-04 16:32:29 +0800

    feat: load paginated data loading

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -7,7 +7,7 @@ import { api } from '../api';
 import { toast } from 'sonner';
 
 function Records() {
-  // ui: add loading icon while ongoing ang loading ng records.
+  // ui: add loading icon while ongoing ang loading ng records..
   const [records, setRecords] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
